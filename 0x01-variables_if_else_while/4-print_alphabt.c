d6c052e1fa2f9c0b7918486b57d9fea8e00412ff
Menelik04 <menelikgete1@gmail.com>
2023-06-17 12:49:46 -0700

    with out q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - to print lowercases except q and e.
+ * discription - using only putchar function.
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char lowercase = 'n';
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if(n != 9 && n != e)
+	{
+		putchar(n);
+	}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
