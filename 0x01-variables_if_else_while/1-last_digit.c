863510cc3eee78555baf1ff2c18cc69eb5c13dd0
Menelik04 <menelikgete1@gmail.com>
2023-06-17 07:01:09 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,10 +11,13 @@
 int main(void)
 {
 	int n, x;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* to store the remined decimal number */
 	x = n % 10;
-	printf("Last digit of %d", n);
+	printf("Last digit of %d ", n);
+	/* to check the numbers sighn */
 	if (x > 5)
 	{
 		printf("is %d greater than 5\n", x);
