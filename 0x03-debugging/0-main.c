111665f5aaaa4a69a2fe9e63db9c116c0397e3cc
Menelik04 <menelikgete1@gmail.com>
2023-06-22 15:45:10 -0700

    0x03. C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#nclude "main.h"
+
+/**
+ * main - a program which test for negative or positive
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return(0);
+}
